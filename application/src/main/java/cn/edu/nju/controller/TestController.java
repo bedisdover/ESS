49c3ffb8291b538a377705ab337c3e867455c006
doversong <doversong@tencent.com>
2017-10-17 16:23:42 +0800

    feature: update log4j config

--- application/src/main/java/cn/edu/nju/controller/TestController.java
+++ application/src/main/java/cn/edu/nju/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.edu.nju.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+    private static Logger logger = Logger.getLogger(TestController.class);
+
     @RequestMapping("/test")
     @ResponseBody
     public String test() {
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+
         return "test application";
     }
 }
