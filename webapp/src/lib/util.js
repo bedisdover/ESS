8018832ffbdf6d32df68acb4de73c88749a72f70
doversong <doversong@tencent.com>
2017-11-01 19:03:40 +0800

    feature: add getParamByName from url into util.js

--- webapp/src/lib/util.js
+++ webapp/src/lib/util.js
@@ -91,4 +91,17 @@ util.validateEmail = (rule, value, callback) => {
   }
 }
 
+/**
+ * 根据参数名称获取url中的参数
+ */
+util.getParamByName = function (name) {
+  let url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
 export default util
