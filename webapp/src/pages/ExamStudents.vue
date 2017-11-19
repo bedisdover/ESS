e7329e88cbb359a6eb719ce3d0777f485fcd1de3
wangzelin <wangzelin@meituan.com>
2017-11-19 13:55:03 +0800

    delete useless css

--- webapp/src/pages/ExamStudents.vue
+++ webapp/src/pages/ExamStudents.vue
@@ -123,20 +123,6 @@
   }
 </script>
 <style scoped>
-  .demo-table-expand {
-    font-size: 0;
-  }
-
-  .demo-table-expand label {
-    color: #99a9bf;
-  }
-
-  .demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
-  }
-
   .deleteButton {
     margin-top: 20px;
     margin-bottom: 20px;
